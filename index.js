3a65c64bb19a19e33ba3606f62b441ba41bae06a
hatnfx19886 <hatnfx19886@funix.edu.vn>
2023-06-15 21:42:30 +0700

    Update Rule

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const body = document.querySelector('body');
 
 let secretNumber = Math.trunc(Math.random() * 100) + 1;
 let score = 20;
+let playing = true;
 let highscore = JSON.parse(localStorage.getItem('high')) || 0;
 if (highscore > 20 || isNaN(Number(highscore))) {
   localStorage.removeItem('high');
@@ -15,35 +16,40 @@ if (highscore > 20 || isNaN(Number(highscore))) {
 highscoreElm.textContent = highscore;
 
 document.querySelector('.check').addEventListener('click', function () {
-  const guess = Number(guessInput.value);
-  if (!guess || guess % 1 !== 0) {
-    messageElm.textContent = '⛔ Invalid Number!';
-  } else if (guess === secretNumber) {
-    messageElm.textContent = '🎉 Correct Number!';
-    body.style.backgroundColor = '#60b347';
-    numberElm.style.width = '200px';
-    numberElm.textContent = secretNumber;
+  if (playing) {
+    const guess = Number(guessInput.value);
+    if (!guess || guess % 1 !== 0) {
+      messageElm.textContent = '⛔ Invalid Number!';
+    } else if (guess === secretNumber) {
+      messageElm.textContent = '🎉 Correct Number!';
+      body.style.backgroundColor = '#60b347';
+      numberElm.style.width = '200px';
+      numberElm.textContent = secretNumber;
+      playing = false;
+      guessInput.setAttribute('disabled', true);
 
-    if (score > highscore) {
-      highscore = score;
-      localStorage.setItem('high', JSON.stringify(highscore));
-      highscoreElm.textContent = highscore;
-    }
-  } else if (guess !== secretNumber && guess >= 1 && guess <= 100) {
-    if (score > 1) {
-      messageElm.textContent =
-        guess > secretNumber ? '📈 Too high!' : '📉 Too low!';
-      score--;
-      scoreElm.textContent = score;
-    } else {
-      messageElm.textContent = '💥 Game Over!';
-      scoreElm.textContent = 0;
-    }
-  } else {
-    messageElm.textContent = '⛔ Between 1 and 100!';
+      if (score > highscore) {
+        highscore = score;
+        localStorage.setItem('high', JSON.stringify(highscore));
+        highscoreElm.textContent = highscore;
+      }
+    } else if (guess !== secretNumber && guess >= 1 && guess <= 100) {
+      if (score > 1) {
+        messageElm.textContent =
+          guess > secretNumber ? '📈 Too high!' : '📉 Too low!';
+        score--;
+        scoreElm.textContent = score;
+      } else {
+        messageElm.textContent = '💥 Game Over!';
+        scoreElm.textContent = 0;
+        playing = false;
+      }
+    } else messageElm.textContent = '⛔ Between 1 and 100!';
   }
 });
+
 document.querySelector('.again').addEventListener('click', function () {
+  playing = true;
   score = 20;
   secretNumber = Math.trunc(Math.random() * 100) + 1;
   messageElm.textContent = 'Start guessing ...';
